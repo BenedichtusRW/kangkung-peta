214593e27894e180f0aa5bf3ae85cab3d080d806
BenedichtusRW <antabrw215@gmail.com>
2026-08-23 13:36:43 +0700

    style: tambahkan jarak dan bullet pada teks berjalan

--- Pages/index.php
+++ Pages/index.php
@@ -109,8 +109,8 @@ $teksPengumuman = !empty($pengumuman_teks) ? $pengumuman_teks : $defaultPengumum
 <div class="announcement-ticker-bar">
   <div class="announcement-track-wrap">
     <div class="announcement-track">
-      <span><?= e($teksPengumuman) ?> &nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;&nbsp;</span>
-      <span><?= e($teksPengumuman) ?> &nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;&nbsp;</span>
+      <span><?= e($teksPengumuman) ?><span style="padding: 0 40px; color: var(--gold-500);">&bull;</span></span>
+      <span><?= e($teksPengumuman) ?><span style="padding: 0 40px; color: var(--gold-500);">&bull;</span></span>
     </div>
   </div>
 </div>
